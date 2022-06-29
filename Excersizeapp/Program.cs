632d7889b86afad7073a7740cd4d0b75c2561100
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-06-29 16:19:01 +0530

    2

--- Excersizeapp/Program.cs
+++ Excersizeapp/Program.cs
@@ -6,38 +6,13 @@ namespace Excersizeapp
     {
         static void Main(string[] args)
         {
-            //integer
-            // declaring a variable
-            int num1;
-            // assigning a value to a variable
-            num1 = 13;
-            //declaring and initializing a variable in one go
-            int num2 = 23;
-            int sum = num1+ num2;
-
-            //using concatination
-            Console.WriteLine("num1 is " + num1);
-            Console.WriteLine("num1: " + num1 + " + num2: " + num2 + " is " + sum);
-
-            //Double
-            double d1 = 3.5342;
-            double d2 = 5.1;
-            double dDiv = d1 / d2;
-            Console.WriteLine("d1/d2 is " + dDiv);
-
-            //Float
-            float f1 = 3.5342f;
-            float f2 = 5.1f;
-            float fDiv = f1/ f2;
-            Console.WriteLine("f1/f2 is " + fDiv);
-
-            //Long
-            long l1 = 532626547869767361;
-
-
-            //diff variable inone 
-            double dIDiv = d1 / num1;
-            Console.WriteLine("d1/num2 is " + dIDiv);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Clear();
+            string name = "Raginee";
+            string message = "My name is  " + name;
+            string capsMessage = message.ToUpper();
+            Console.WriteLine(capsMessage);
 
             Console.Read();
         }
