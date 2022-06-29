37e7e563204d02e7855244e3973e341cfdf5cb97
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-06-29 14:39:39 +0530

    Add project files.

---
+++ Excersizeapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Excersizeapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
