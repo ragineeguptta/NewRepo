95845065bff786e831ee6bcbc0f32e33f85445ed
Raginee <raginee.gupta@hirdhav.com>
2022-08-19 18:35:21 +0530

    1

---
+++ HelloWorldd/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorldd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteSomething();
+            WriteSomethingSpecific("I am an argument and called from a method");
+            Console.Read();
+        }
+
+        //accessmodifier, (static), retun type, method name (parameter1)
+        public static void WriteSomething()
+        {
+            Console.WriteLine("I am called by method");
+        }
+
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
+        }
+    }
+}
