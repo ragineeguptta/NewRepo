13dde697204eafe73a865b6755d962cd4fa38fa4
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-08-22 18:33:12 +0530

    1

---
+++ oprators/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace oprators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num3;
+            // unary operators
+            num3 = -num1;
+            Console.WriteLine("num3 is {0}",num3);
+
+            bool isSunny = true;
+            Console.WriteLine("is it Sunny? {0}", !isSunny);
+
+            // increment operators
+            num3 = 0;
+            num3++;
+            Console.WriteLine("num3 is {0}", num3);
+            Console.WriteLine("num3 is {0}", num3++);
+            // pre increament oprATOR
+            Console.WriteLine("num3 is {0}", ++num3);
+
+            // DEcrement operators
+            num3 = 0;
+            num3--;
+            Console.WriteLine("num3 is {0}", num3);
+            Console.WriteLine("num3 is {0}", num3--);
+            // pre DEcrement oprATOR
+            Console.WriteLine("num3 is {0}", --num3);
+
+
+            Console.ReadKey();
+        }
+    }
+}
