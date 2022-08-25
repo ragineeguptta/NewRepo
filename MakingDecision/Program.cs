776b63294b3cb4514c0cbaaeb90ac7bae0f2c5e0
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-08-25 12:20:07 +0530

    basic decision making

---
+++ MakingDecision/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MakingDecision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int temperature = 10;
+
+            //if (temperature < 10)
+            //{
+            //    Console.WriteLine("Take the coat");
+            //}
+
+            //if (temperature == 10)
+            //{
+            //    Console.WriteLine("The temp is 10 degree");
+            //}
+            //if (temperature > 10)
+            //{
+            //    Console.WriteLine("Cozy and Warm");
+            //}
+
+            Console.WriteLine("What's the temperature like?");
+            string temperature = Console.ReadLine();
+            int numTemp = int.Parse(temperature);
+
+            if(numTemp < 20)
+            {
+                Console.WriteLine("Take the Coat");
+            }
+
+            else if (numTemp == 20)
+            {
+                Console.WriteLine("Pants and Pull Over should be fine");
+            }
+
+            else
+            {
+                Console.WriteLine("Short are enough today");
+            }
+
+            Console.Read();
+        }
+    }
+}
