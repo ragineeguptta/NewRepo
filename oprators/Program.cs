f52df9917703deb3ae89494198bbd7aeb80e7bd5
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-08-25 11:48:23 +0530

    done

--- oprators/Program.cs
+++ oprators/Program.cs
@@ -7,6 +7,7 @@ namespace oprators
         static void Main(string[] args)
         {
             int num1 = 5;
+            int num2 = 3;
             int num3;
             // unary operators
             num3 = -num1;
@@ -24,13 +25,50 @@ namespace oprators
             Console.WriteLine("num3 is {0}", ++num3);
 
             // DEcrement operators
-            num3 = 0;
             num3--;
             Console.WriteLine("num3 is {0}", num3);
             Console.WriteLine("num3 is {0}", num3--);
             // pre DEcrement oprATOR
             Console.WriteLine("num3 is {0}", --num3);
 
+
+            int result;
+
+            result = num1 + num2;
+            Console.WriteLine("result of num1 + num2 is {0}", result);
+            result = num1 - num2;
+            Console.WriteLine("result of num1 - num2 is {0}", result);
+            result = num1 * num2;
+            Console.WriteLine("result of num1 * num2 is {0}", result);
+            result = num1 / num2;
+            Console.WriteLine("result of num1 / num2 is {0}", result);
+            result = num1 % num2;
+            Console.WriteLine("result of num1 % num2 is {0}", result);
+
+            //relational and type oprators
+            bool isLower;
+            isLower = num1 > num2;
+            Console.WriteLine("result of num1 >num2 is {0}", isLower);
+
+            //equality oprator
+
+            bool isEqual;
+            isEqual = num1 == num2;
+            Console.WriteLine("result of num1 == num2 is {0}", isEqual);
+
+            isEqual = num1 != num2;
+            Console.WriteLine("result of num1 != num2 is {0}", isEqual);
+
+            // conditional operators
+            bool isLowerAndSunny;
+            // conditional AND operators
+            isLowerAndSunny = isLower && isSunny;
+            Console.WriteLine("result od isLower && isSunny is {0} ", isLowerAndSunny);
+
+
+            // conditional OR operators
+            isLowerAndSunny = isLower || isSunny;
+            Console.WriteLine("result od isLower || isSunny is {0} ", isLowerAndSunny);
 
             Console.ReadKey();
         }
