25b9e9f4adec197393b37716845567c936b7cb44
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-08-26 17:26:43 +0530

    1

--- MakingDecision/Program.cs
+++ MakingDecision/Program.cs
@@ -6,6 +6,36 @@ namespace MakingDecision
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What's the temperature like?");
+            string temperature = Console.ReadLine();
+            int numTemp;
+            int number;
+            if(int.TryParse(temperature, out number))
+            {
+                numTemp = number;
+            }
+            else
+            {
+                numTemp = 0;
+                Console.WriteLine("Value entered, was no number. 0 set as temperature.");
+            }
+
+            //string numberAsString = "128";
+            //float parsedValue;
+
+            //bool success = float.TryParse(numberAsString, out parsedValue);
+
+            //if (success)
+            //{
+            //    Console.WriteLine("Parsing successful - number is " + parsedValue);
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Parsing failed");
+            //}
+
+
             //int temperature = 10;
 
             //if (temperature < 10)
@@ -22,24 +52,26 @@ namespace MakingDecision
             //    Console.WriteLine("Cozy and Warm");
             //}
 
-            Console.WriteLine("What's the temperature like?");
-            string temperature = Console.ReadLine();
-            int numTemp = int.Parse(temperature);
 
-            if(numTemp < 20)
-            {
-                Console.WriteLine("Take the Coat");
-            }
 
-            else if (numTemp == 20)
-            {
-                Console.WriteLine("Pants and Pull Over should be fine");
-            }
+            //Console.WriteLine("What's the temperature like?");
+            //string temperature = Console.ReadLine();
+            //int numTemp = int.Parse(temperature);
 
-            else
-            {
-                Console.WriteLine("Short are enough today");
-            }
+            //if(numTemp < 20)
+            //{
+            //    Console.WriteLine("Take the Coat");
+            //}
+
+            //else if (numTemp == 20)
+            //{
+            //    Console.WriteLine("Pants and Pull Over should be fine");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Short are enough today");
+            //}
 
             Console.Read();
         }
