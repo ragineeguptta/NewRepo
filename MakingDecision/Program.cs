519c1c077de040df7517b7cfb62df65a7b8b1ec3
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-08-30 16:36:01 +0530

    3

--- MakingDecision/Program.cs
+++ MakingDecision/Program.cs
@@ -4,26 +4,50 @@ namespace MakingDecision
 {
     class Program
     {
+        static int highscore = 300;
+        static string highplayerName = "Raginee";
         static void Main(string[] args)
         {
-            //Switch Statement
+            CheckHighScore(250, "aa");
+            CheckHighScore(350, "bb");
+            CheckHighScore(330, "cc");
 
-            int age = 79;
-
-            switch (age)
-            {
-                case 15:
-                    Console.WriteLine("Too young");
-                    break;
-                case 25:
-                    Console.WriteLine("Good to go");
-                    break;
-                default:
-                    Console.WriteLine("Too old");
-                    break;
-
-            }
             Console.Read();
+
+            ////Switch Statement
+
+            //int age = 79;
+
+            //switch (age)
+            //{
+            //    case 15:
+            //        Console.WriteLine("Too young");
+            //        break;
+            //    case 25:
+            //        Console.WriteLine("Good to go");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Too old");
+            //        break;
+
+            //}
+            //Console.Read();
+        }
+        public static void CheckHighScore(int score, string playerName)
+        {
+            if(score > highscore)
+            {
+                highscore = score;
+                highplayerName = playerName;
+
+                Console.WriteLine("New highscore is " + score);
+                Console.WriteLine("It is now held by " + playerName);
+            }
+            else
+            {
+
+                Console.WriteLine("The old highscore could not be broken. It is still " + highscore + " and held by " + highplayerName);
+            }
         }
     }
 }
