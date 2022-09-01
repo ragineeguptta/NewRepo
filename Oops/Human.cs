843d6b740927a46d852ff04d15a45270a5f7049e
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-09-01 14:38:40 +0530

    1

---
+++ Oops/Human.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops
+{
+    // add two more member variables to "Human". eyeColor and age.
+    // adjust the constructor, so it requires all of the four values to be used, when creating an object of Human.
+    // create two objects of type Human.
+
+    // this class is a blueprint for a datatype 
+    class Human
+    {
+        // member variable
+        private string firstName;
+        private string lastName;
+        private string eyeColor;
+        private int age;
+
+        // default constructor
+        public Human()
+        {
+            Console.WriteLine("Constructor called. Object created");
+        }
+
+        public Human(string firstName)
+        {
+            this.firstName = firstName;
+        }
+
+        public Human(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public Human(string firstName, string lastName, string eyeColor)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+        }
+
+        public Human(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        // parameterized constructor
+        public Human(string firstName, string lastName, string eyeColor, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+            this.age = age;
+        }
+
+
+
+        // member method
+        public void IntroduceMyself()
+        {
+            if (age != 0 && lastName != null && eyeColor != null && firstName != null)
+                Console.WriteLine("Hi, I'm {0} {1} and {2} years old. My eye color is {3}", firstName, lastName, age, eyeColor);
+            else if (age != 0 && lastName != null && firstName != null)
+            {
+                Console.WriteLine("Hi, I'm {0} {1} and {2} years old.", firstName, lastName, age);
+            }
+            else if (lastName != null && firstName != null)
+            {
+                Console.WriteLine("Hi, I'm {0} {1}.", firstName, lastName);
+            }
+            else if (firstName != null)
+            {
+                Console.WriteLine("Hi, I'm {0} ", firstName);
+            }
+
+        }
+    }
+}
