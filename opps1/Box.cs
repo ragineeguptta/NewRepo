ac4431f5949105ac9c555dca607b0446aa3a390d
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-09-06 16:19:13 +0530

    1

---
+++ opps1/Box.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace opps1
+{
+    class Box
+    {
+        //member variable
+        private int lenght = 3;
+        public int height;
+        public int width;
+        public int volume;
+
+
+        public void SetLength(int lenght)
+        {
+            this.lenght = lenght;
+        }
+
+        public int GetLenght()
+        {
+            return this.lenght;
+        }
+
+        public int GetVolumn()
+        {
+            return this.lenght*this.height*this.width;
+        }
+
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Length is {0} and height is {1} and width is {2} so volumn is {3}", lenght, height, width, volume = lenght*height*width);
+        }
+
+    }
+}
---
+++ opps1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace opps1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box();
+            // box.lenght = 3;
+            box.SetLength(15);
+            box.height = 4;
+            box.width = 5;
+
+            Console.WriteLine("Box lenght is " + box.GetLenght());
+
+            Console.WriteLine("Box volumn is " + box.GetVolumn());
+            box.DisplayInfo();
+
+        }
+    }
+}
