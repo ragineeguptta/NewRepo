ac866cef50b06788d377d06afba016322b710d76
Raginee Gupta <raginee.gupta@hirdhav.com>
2022-09-23 16:49:02 +0530

    multidimesional array

--- 7-Array/Program.cs
+++ 7-Array/Program.cs
@@ -6,70 +6,63 @@ namespace _7_Array
     {
         static void Main(string[] args)
         {
-            Boolean valid = false;
-            string inputValueType;
+            //declare 2D Array
+            string[,] matrix;
 
-            Console.Write("Enter a Value :");
-            string inputValue = Console.ReadLine();
+            // 3D array
+            int[,,] threeD;
 
-
-            Console.Write("Select the Data type to validate the input you have entered");
-            Console.Write("Press 1 for String");
-            Console.Write("Press 2 for Integer");
-            Console.Write("Press 3 for Boolean");
-
-
-            Console.Write("Enter :");
-            int inputType = Convert.ToInt32(Console.ReadLine());
-
-            switch (inputType)
+            // two dimensional array
+            int[,] array2D = new int[,]
             {
-                case 1:
-                    //check for string
-                    valid = IsAllAlphabetic(inputValue);
-                    inputValueType = "String";
-                    break;
-                case 2:
-                    int retValue = 0;
-                    //check for integer
-                    valid = int.TryParse(inputValue, out retValue);
-                    inputValueType = "Integer";
-                    break;
-                case 3:
-                    bool retFlag = false;
-                    //check for Boolean
-                    valid = bool.TryParse(inputValue, out retFlag);
-                    inputValueType = "Boolean";
-                    break;
+                {1,2,3 }, // row 0
+                {4,5,6 }, // row 1
+                {7,8,9 } // row 2
+            };
 
-                default:
-                    inputValueType = "Unknown";
-                    Console.WriteLine("Not able to detect the input type, something is wrong");
-                    break;
-            }
+            Console.WriteLine("Central value is {0}", array2D[1, 1]);
+            Console.WriteLine("Central value is {0}", array2D[2, 0]);
 
-            Console.WriteLine("You have entered a value : {0}", inputValue);
-            if (valid)
+
+            // 3 dimensional array
+            string[,,] array3D = new string[,,]
             {
-                Console.WriteLine("It is valid : {0}", inputValue);
-            }
-            else
-            {
-                Console.WriteLine("It is an invalid : {0}", inputValueType);
-            }
+                {
+                    {"000", "001" },
+                    {"010", "011" }
+                },
+                {
+                    {"100", "101" },
+                    {"110", "111" }
+                }
+            };
 
+
+            Console.WriteLine("The value is {0}", array3D[1,1, 0]);
+
+            //2D string
+
+            string[,] array2DString = new string[3, 2]
+            {
+                {"one", "two" },
+                {"three", "four" },
+                {"five", "six" }
+
+            };
+
+            array2DString[1, 1] = "chicken";
+
+            //Rank return the amount of dimensions
+            int dimensions = array2DString.Rank;
+
+            //without specifying new things
+            int[,] array2D2 = { { 1, 2 }, { 3, 4 } };
+
+            Console.WriteLine("dimensions value is {0}", dimensions);
+            Console.WriteLine("Central value is {0}", array2DString[1, 1]);
             Console.ReadKey();
+
         }
 
-        static bool IsAllAlphabetic(string value)
-        {
-            foreach(char c in value)
-            {
-                if (!char.IsLetter(c))
-                    return false;
-            }
-
-            return true;
-        }
     }
 }
