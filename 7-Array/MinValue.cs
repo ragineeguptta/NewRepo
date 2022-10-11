2bbf77b9d58d7efd6f5f79f3053db5573e1c0012
Raginee Gupta <ragineegupta123@gmail.com>
2022-10-11 12:33:18 +0530

    using params for min value

--- 7-Array/MinValue.cs
+++ 7-Array/MinValue.cs
@@ -8,7 +8,21 @@ namespace _7_Array
     {
         static void Main(string[] args)
         {
+            int min = minV(6, 5, 3, 0, -4, 8);
+
+            Console.WriteLine("The minimum is :{0} ", min);
         }
 
+        public static int minV(params int[] numbers)
+        {
+            int min = int.MaxValue;
+
+            foreach(int number in numbers)
+            {
+                if (number < min)
+                    min = number;
+            }
+            return min;
         }
+    }
 }
