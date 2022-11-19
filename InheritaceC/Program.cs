b87eb35dfc85cc52887935fe425e3b7a8ddaedc6
Raginee Gupta <raginee123@gmail.com>
2022-11-19 15:57:22 +0530

    Inheritance demo

---
+++ InheritaceC/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InheritaceC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImagePost imagePost = new ImagePost("Check out my new shoes", "Denis Panjuta", "https://image.com/shoes", true);
+            Post post1 = new Post("Thanks for the birthday wishes", true, "Denis Panjuta");
+            Console.WriteLine(post1.ToString());
+            Console.WriteLine(imagePost.ToString());
+            Console.ReadLine();
+        }
+    }
+}
