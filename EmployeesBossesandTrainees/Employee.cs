e432683b4f4e20a1c9a3d4702b7e0d13192ab218
Raginee Gupta <raginee123@gmail.com>
2022-11-19 17:31:41 +0530

    Task

---
+++ EmployeesBossesandTrainees/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeesBossesandTrainees
+{
+    class Employee
+    {
+        protected string Name { get; set; }
+        protected string FirstName { get; set; }
+        protected int Salary { get; set; }
+
+        public void Work()
+        {
+
+        }
+        public void Learn()
+        {
+
+        }
+
+    }
+}
