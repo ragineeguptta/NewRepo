ef45be554bcd2bc50ae65e73ba9dfcefb8be4ff3
Raginee Gupta <raginee123@gmail.com>
2022-11-19 12:54:39 +0530

    1

---
+++ InheritanceDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace InheritanceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Redio myRadio = new Redio(false,"Sony");
+            myRadio.SwitchOn();
+            myRadio.ListenRadio();
+
+            
+            TV myTV = new TV(false, "Samsung");
+            //method of parent class
+            myTV.SwitchOn();
+            // method of child class
+            myTV.WatchTV();
+        }
+    }
+}
---
+++ InheritanceDemo/TV.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceDemo
+{
+    class TV : ElectricalDevice
+    {
+        
+        public TV(bool isOn, string brand):base(isOn, brand)
+        {
+           
+        }
+
+        //method to watch to the redio
+        public void WatchTV()
+        {
+            //first check if the tv is on 
+            if (IsOn)
+            {
+                //watch to tv
+                Console.WriteLine("Watching TV!");
+            }
+            else
+            {
+                //print error message
+                Console.WriteLine("TV is switched off, switch it on first");
+            }
+        }
+    }
+}
