96f688b3c4aa188e7700639e0a8c216dc8248b2d
Raginee Gupta <raginee123@gmail.com>
2022-11-21 16:59:06 +0530

    Abstract and as & is Keyword / Polymorphism

--- AbstractClassesC/Program.cs
+++ AbstractClassesC/Program.cs
@@ -12,6 +12,22 @@ namespace AbstractClassesC
             {
                 shape.GetInfo();
                 Console.WriteLine("{0} has a volumn of {1}", shape.Name, shape.Volume());
+
+                Cube iceCube = shape as Cube;
+                if(iceCube == null)
+                {
+                    Console.WriteLine("This shape is no cube");
+                }
+
+                if(shape is Cube)
+                {
+
+                    Console.WriteLine("This shape is a cube");
+                }
+
+                object cube1 = new Cube(7);
+                Cube cube2 = (Cube)cube1;
+                Console.WriteLine("{0} has a volumn of {1}", cube2.Name,cube2.Volume());
             }
         }
     }
