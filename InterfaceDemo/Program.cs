bfb221b0fa6a39f516499ae998881b5e23b4937f
Raginee Gupta <raginee123@gmail.com>
2022-11-21 11:37:09 +0530

    Interface Demo1

---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InterfaceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ticket t1 = new Ticket(10);
+            //Ticket t2 = new Ticket(10);
+            //Console.WriteLine(t2.Equals(t1));
+        }
+    }
+}
---
+++ InterfaceDemo/Ticket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceDemo
+{
+    class Ticket : IEquatable<Ticket>
+    {
+        //property to store the duration of the ticket in hours
+        public int DurationInHours { get; set; }
+        //simple constructor
+        public Ticket(int durationInHours)
+        {
+            DurationInHours = durationInHours;
+        }
+
+        public bool Equals(Ticket other)
+        {
+            return this.DurationInHours == other.DurationInHours;
+        }
+    }
+}
