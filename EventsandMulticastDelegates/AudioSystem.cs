0e79191b69e43fa20e0307c241385c284357667d
Raginee Gupta <raginee123@gmail.com>
2022-11-24 12:54:07 +0530

    Events and Multicast Delegates

---
+++ EventsandMulticastDelegates/AudioSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventsandMulticastDelegates
+{
+    class AudioSystem
+    {
+        //simple constructor
+        public AudioSystem()
+        {
+            //simple to the OnGameStart and OnGameOver events.
+            GameEventManager.OnGameStart += StartGame;
+            GameEventManager.OnGameOver += GameOver;
+        }
+
+        //at the start of the game , we want to enable the audio system amnd start playing audio clops
+        private void StartGame()
+        {
+            Console.WriteLine("Audio System Started ");
+            Console.WriteLine("Playing Audio........");
+        }
+
+        //when the game is over, we want to stop our rendring engine
+        private void GameOver()
+        {
+            Console.WriteLine("Audio System Stopped");
+        }
+    }
+}
---
+++ EventsandMulticastDelegates/GameEventManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventsandMulticastDelegates
+{
+    class GameEventManager
+    {
+        //a new delegate tye called GameEvent
+        public delegate void GameEvent();
+
+        //create two delegates called OnGameStart and OnGameOver
+        public static event GameEvent OnGameStart, OnGameOver;
+
+        //a static MethodAccessException to trigger OnGameStart
+        public static void TrigggerGameStart()
+        {
+            //check if the OnGameStart event is not empty,
+            //meaning that other method already subscribed to it
+            if(OnGameStart != null)
+            {
+                //print a simple message
+                Console.WriteLine("The game has started ......");
+                //call the OnGameStart that will trigger all the methods subscribed to this event
+                OnGameStart();
+            }
+        }
+
+        //a static Method to trigger OnGameOver
+        public static void TrigggerGameOver()
+        {
+            //check if the TrigggerGameOver event is not empty,
+            //meaning that other method already subscribed to it
+            if (OnGameOver != null)
+            {
+                //print a simple message
+                Console.WriteLine("The game has started ......");
+                //call the OnGameStart that will trigger all the methods subscribed to this event
+                OnGameOver();
+            }
+        }
+
+    }
+}
